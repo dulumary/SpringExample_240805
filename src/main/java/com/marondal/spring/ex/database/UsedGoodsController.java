15199f1ff9d6c15b8c0cd2b1e6352110422abb60
megait <megait@gmail.com>
2024-08-07 21:41:33 +0900

    [김인규]  - database 연동 mvc 예제

---
+++ src/main/java/com/marondal/spring/ex/database/UsedGoodsController.java
@@ -0,0 +1,32 @@
+package com.marondal.spring.ex.database;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.marondal.spring.ex.database.domain.UsedGoods;
+import com.marondal.spring.ex.database.service.UsedGoodsService;
+
+// request와 response 처리 담당
+@Controller
+public class UsedGoodsController {
+	
+	@Autowired
+	private UsedGoodsService usedGoodsService;
+	
+	@RequestMapping("/db/usedgoods/list")
+	@ResponseBody
+	public List<UsedGoods> usedGoodsList() {
+		
+		// 중고물품 게시글 리스트 얻어 오기 
+		List<UsedGoods> usedGoodsList = usedGoodsService.getUsedGoodsList();
+		
+		
+		return usedGoodsList;
+	}
+	
+
+}
---
+++ src/main/java/com/marondal/spring/ex/database/repository/UsedGoodsRepository.java
@@ -0,0 +1,18 @@
+package com.marondal.spring.ex.database.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.marondal.spring.ex.database.domain.UsedGoods;
+
+// 데이터베이스 관련 처리 
+// 쿼리 수행
+@Mapper
+public interface UsedGoodsRepository {
+	
+	// usedGoods 모든 행 조회 
+	public List<UsedGoods> selectUsedGoodsList();
+	
+
+}
