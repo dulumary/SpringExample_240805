f13d864125c076cda704252fd82642f42c9ca112
megait <megait@gmail.com>
2024-08-09 20:54:58 +0900

    [김인규]  - mybatis insert 예제

--- src/main/java/com/marondal/spring/ex/mybatis/repository/ReviewRepository.java
+++ src/main/java/com/marondal/spring/ex/mybatis/repository/ReviewRepository.java
@@ -10,5 +10,15 @@ public interface ReviewRepository {
 	
 	// 전달 받은 id와 일치하는 행 조회 
 	public Review selectReview(@Param("id") int id);
-
+	
+	// 리뷰테이블에 한행 저장에 필요한 정보를 전달 받고 한 행을 insert
+	public int insertReview(
+			@Param("storeId") int storeId
+			, @Param("menu") String menu
+			, @Param("userName") String userName
+			, @Param("point") double point
+			, @Param("review") String review);
+	
+	// 리뷰테이블에 한행 저장에 필요한 정보를 객체로 전달 받고 한행을 insert
+	public int insertReviewByObject(Review review);
 }
